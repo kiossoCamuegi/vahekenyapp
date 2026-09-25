4ffc31276691fc550b6679cfef4b3f520e91e0e0
kiossoCamuegi <kiossocamuegi@gmail.com>
2026-09-25 19:05:21 +0100

    02: authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import { dark, neobrutalism } from "@clerk/ui/themes";
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -19,18 +21,35 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${outfit.variable} antialiased`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+
+    <ClerkProvider appearance={{ theme: dark ,     signIn: { theme: neobrutalism },}}>
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${outfit.variable} antialiased`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <header>
+              <Show when="signed-out">
+                <SignInButton />
+                <SignUpButton>
+                  <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </Show>
+              <Show when="signed-in">
+                <UserButton />
+              </Show>
+            </header>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+
+    </ClerkProvider>
   );
 }
- 
+
